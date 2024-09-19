1d1130e0f184d2e5f10881be7db7fcb43db01479
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2024-09-20 01:36:23 +0200

    feat: added a ref into image card

--- src/components/image-card.tsx
+++ src/components/image-card.tsx
@@ -2,24 +2,28 @@ import Image from "next/image";
 import { TbDownload as DownloadIcon } from "react-icons/tb";
 import { RxAvatar as Avatar } from "react-icons/rx";
 import {ImageType} from "@/types";
+import React from "react";
 
 interface ImageCardProps {
     image: ImageType
 }
 
-export default function ImageCard({image}: ImageCardProps) {
+const ImageCard = React.forwardRef<HTMLDivElement, ImageCardProps>(({image}, ref) => {
     return (
-        <div className="image-card cursor-pointer mb-2 break-inside-avoid relative group">
+        <div
+            ref={ref}
+            className="image-card cursor-pointer mb-2 break-inside-avoid relative group">
             <Image
                 src={image.src.original}
                 alt={image.alt}
                 width={image.width} height={image.height}
             />
 
-            <div className="absolute z-10 hidden group-hover:flex group-hover:visible flex-wrap gap-y-3 items-center justify-between bottom-3 left-3 right-3">
+            <div
+                className="absolute z-10 hidden group-hover:flex group-hover:visible flex-wrap gap-y-3 items-center justify-between bottom-3 left-3 right-3">
                 <div className="flex items-center space-x-3">
                     <span className="w-10 h-10 rounded-full bg-[#07A081FF] flex items-center justify-center text-white">
-                        <Avatar size={30} />
+                        <Avatar size={30}/>
                     </span>
                     <h3 className="text-white font-semibold text-lg">{image.photographer}</h3>
                 </div>
@@ -31,4 +35,8 @@ export default function ImageCard({image}: ImageCardProps) {
             </div>
         </div>
     )
-}
+})
+
+ImageCard.displayName = "ImageCard"
+
+export default ImageCard;
