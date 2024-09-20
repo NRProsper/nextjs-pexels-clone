f31884d0ab644dc112409024b54df001117bc7d2
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2024-09-20 12:57:48 +0200

    feat: added a search wrapper and search for header

--- src/components/layout/wrapper.tsx
+++ src/components/layout/wrapper.tsx
@@ -1,4 +1,5 @@
 import {ReactNode} from "react";
+import WrapperSearch from "@/components/layout/wrapper-search";
 
 export default function ChildrenWrapper(
     {children}: {
@@ -7,8 +8,8 @@ export default function ChildrenWrapper(
 ) {
     return(
         <>
-            <header className="min-h-[500px] relative flex items-center justify-center after:absolute after:inset-0 after:bg-black/50 after:z-[1">
-                <div className="w-full max-w-[630px] text-center z-[2]">
+            <header className="min-h-[500px]  relative flex flex-col items-center justify-center after:absolute after:inset-0 after:bg-black/50 after:z-[1">
+                <div className="w-full text-center z-[2] max-w-[630px]">
                     <h1 className="text-3xl text-white font-semibold">The best free stock photos, royalty free images & videos shared by creators.</h1>
                 </div>
                 <img
@@ -16,6 +17,7 @@ export default function ChildrenWrapper(
                     alt={"header image"}
                     className="w-full h-full absolute top-0 left-0 -z-[1] object-cover"
                 />
+                <WrapperSearch />
             </header>
             <main>
                 {children}
